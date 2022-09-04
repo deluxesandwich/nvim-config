f8448f7ebdf0d91c524f27d6328abfe5d430d8e4
Alex You Shao <alexshao1993@gmail.com>
2022-09-04 16:45:28 -0700

    updated keymaps

--- after/plugin/which-key.lua
+++ after/plugin/which-key.lua
@@ -10,7 +10,6 @@ which_key.setup({
 })
 
 which_key.register({
-	w = { "<cmd>update!<CR>", "Save" },
 	q = { "<cmd>qa!<CR>", "Quit" },
 	["/"] = { "<cmd>ToggleTerm<CR>", "Toggle terminal" },
 
@@ -25,26 +24,35 @@ which_key.register({
 		R = { "<cmd>Telescope lsp_incoming_calls<cr>", "Read usage" },
 		w = { "<cmd>Telescope lsp_outgoing_calls<cr>", "Write usage" },
 		d = { "<cmd>Telescope lsp_definitions<cr>", "Definitions" },
-		D = { "<cmd>Telescope lsp_diagnostics<cr>", "Diagnostics" },
 		t = { "<cmd>Telescope lsp_type_definitions<cr>", "Type definitions" },
 		i = { "<cmd>Telescope lsp_implementation<cr>", "Implementation" },
 	},
 
-	f = {
-		name = "Telescope",
+	["<leader>"] = {
+		name = "Search - Telescope",
 		f = { "<cmd>Telescope find_files<cr>", "Find files" },
 		g = { "<cmd>Telescope live_grep<cr>", "Grep" },
-		b = { "<cmd>Telescope current_buffer_fuzzy_find<cr>", "Search current buffer" },
+		b = { "<cmd>Telescope buffers<cr>", "Buffers" },
+		c = { "<cmd>Telescope current_buffer_fuzzy_find<cr>", "Search current buffer" },
 		z = { "<cmd>Telescope builtin<cr>", "Builtin pickers" },
 		h = { "<cmd>Telescope help_tags<cr>", "Help" },
 		k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
 	},
 
-	b = {
-		name = "Buffer",
-		c = { "<Cmd>bd!<Cr>", "Close current buffer" },
-		D = { "<Cmd>%bd|e#|bd#<Cr>", "Delete all buffers" },
+	f = {
+		name = "File / Buffer",
+		d = { "<Cmd>bd!<Cr>", "Close current buffer" },
+		s = { "<Cmd>w<Cr>", "Save current buffer" },
+		p = { "<Cmd>bprevious<Cr>", "Previous buffer" },
+		n = { "<Cmd>bnext<Cr>", "Next buffer" },
 	},
+
+    T = {
+        name = "Tab",
+        d = { "<Cmd>tabclose<Cr>"},
+        p = { "<Cmd>tabprevious<Cr>"},
+        n = { "<Cmd>tabnext<Cr>"},
+    },
 
 	t = {
 		name = "nvim-tree",
